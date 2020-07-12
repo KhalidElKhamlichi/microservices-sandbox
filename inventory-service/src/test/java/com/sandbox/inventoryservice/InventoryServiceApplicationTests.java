851d9b5ea0310a88b3d68a89cdba87b1e071c764
Khalid El Boukhari <khalidelboukhari.k@gmail.com>
2020-07-12 23:13:17 +0100

    Add inventory-service

---
+++ inventory-service/src/test/java/com/sandbox/inventoryservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandbox.inventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
